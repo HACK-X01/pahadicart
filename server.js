f30d88ed481bd1b5164ac690d987da37b77bf19b
HACK-X01 <devgurjar9196@gmail.com>
2026-09-08 20:59:11 +0530

    fix(mobile): force app-container display block, fixed bottom nav, no-cache headers and v2.6.0 asset versioning

--- server.js
+++ server.js
@@ -55,7 +55,9 @@ const server = http.createServer((req, res) => {
     } else {
       res.writeHead(200, {
         'Content-Type': contentType,
-        'Cache-Control': 'public, max-age=3600',
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0',
         'Access-Control-Allow-Origin': '*'
       });
       res.end(content);
